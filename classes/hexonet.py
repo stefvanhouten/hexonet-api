0ba35575745cdc92c9b81cf884bf826e75a780d3
Stef <stefvanhouten@gmail.com>
2020-01-10 12:59:58 +0100

    Setup basics for hexonet API communication

---
+++ classes/hexonet.py
@@ -0,0 +1,22 @@
+
+class Customer(object):
+  def __init__(self, companyName, firstName, lastName):
+    self.COMPANY_NAME = companyName
+    self.FIRST_NAME = firstName
+    self.LAST_NAME = lastName
+
+class Hexonet(object):
+
+  @classmethod
+  def add_domain(cls):
+    """ """
+    
+  @classmethod
+  def delete_domain(cls):
+    """ """
+    
+  @classmethod
+  def status_domain(cls):
+    """ """
+    
+    
