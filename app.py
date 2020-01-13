0985883026fab4864b14ec381b41358611c32513
Stef <stefvanhouten@gmail.com>
2020-01-13 10:35:51 +0100

    added functionality to add domain, check status and check multiple domains availability

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-from classes.hexonet import Hexonet
+from classes.hexonet import Hexonet, Customer
+import pprint
+
+pp = pprint.PrettyPrinter(indent=4)
+customer = Customer('stef', 'van Houten', 'emmakade', 'leeuwarden', 'friesland', '8922', 'NL', '0640811668', 'stefvanhouten@gmail.com')
 
 hexonet = Hexonet()
-print(hexonet.check_domain("stef.org"))
+pp.pprint(hexonet.check_domain('test.org'))
+pp.pprint(hexonet.check_domains(['test.org', 'test1.org', 'test2.org', 'test3.org']))
+pp.pprint(hexonet.status_domain('test.org'))
+pp.pprint(hexonet.add_domain('stef5.org', 1, customer, customer, customer, customer, ['nameserver1.com']))
--- classes/hexonet.py
+++ classes/hexonet.py
@@ -1,13 +1,61 @@
 
 import requests
+from functools import update_wrapper, partial
+import pprint
+
+pp = pprint.PrettyPrinter(indent=4)
+
+class MyDecorator(object):
+  def __init__(self, func):
+    update_wrapper(self, func)
+    self.func = func
+
+  def __get__(self, obj, objtype):
+    """Support instance methods."""
+    return functools.partial(self.__call__, obj)
+
+  def __call__(self, obj, *args, **kwargs):
+    response = self.func(obj, *args, **kwargs)
+    text = response.text
+    
+    response_dict = {}
+    start = 0
+    for i, char in enumerate(text):
+      if char == "\n":
+        line = text[start:i].split("=")
+        if len(line) >= 2 and line[1] != "":
+          key, value = line
+          if key in response_dict.keys():
+            key += "_1"
+          response_dict[key] = value
+        start = i + 1
+
+    response_dict['status'] = response.status_code
+    return response_dict
+
+convert_response_to_dict = MyDecorator
+   
 
 class Customer(object):
-    def __init__(self, companyName,
-                 firstName, lastName):
-        self.COMPANY_NAME = companyName
-        self.FIRST_NAME = firstName
-        self.LAST_NAME = lastName
-
+    def __init__(self, firstName, lastName, 
+                 street, city, state, 
+                 zipcode, country, phone, 
+                 email, fax=None, organization=None, 
+                 middlename=None, title=None):
+      
+        self.firstname = firstName
+        self.middlename = middlename
+        self.lastname = lastName
+        self.organization = organization
+        self.street = street,
+        self.city = city
+        self.state = state,
+        self.zip = zipcode
+        self.country = country
+        self.phone = phone
+        self.email = email
+        self.fax = fax
+        self.title = title
 
 class Hexonet(object):
     LOGIN = "stefvanhouten"
@@ -19,11 +67,37 @@ class Hexonet(object):
       's_login': LOGIN,
       's_pw': PW,
     }
-
+    
+    @classmethod 
+    def build_params(cls, name, customer, params):
+      if type(customer) == Customer:
+        for key in customer.__dict__.keys():
+          params['{}0{}'.format(name, key)] = customer.__dict__[key]
+      else:
+        #If its not a customer object its a list of nameservers
+        for i, nameserver in enumerate(customer):
+          params['{}{}'.format(name, i)] = nameserver
+      return params
+    
     @classmethod
-    def add_domain(cls):
-      """ """
-
+    @convert_response_to_dict
+    def add_domain(cls, domain, period, 
+                   owner_contact, admin_contact, tech_contact, 
+                   billing_contact, nameservers=None):
+      """ Used to register new domain names """
+      params = cls.PARAMS.copy()
+      params['command'] = 'AddDomain'
+      params['domain'] = domain
+      params = cls.build_params('ownercontact', owner_contact, params)
+      params = cls.build_params('admincontact', admin_contact, params)
+      params = cls.build_params('techcontact', tech_contact, params)
+      params = cls.build_params('billingcontact', billing_contact, params)
+      
+      if nameservers is not None:
+        params = cls.build_params('nameserver', nameservers, params)
+        
+      return requests.get(cls.API, params=params)
+    
     @classmethod
     def delete_domain(cls):
       """ """
@@ -33,34 +107,39 @@ class Hexonet(object):
       """ """
 
     @classmethod
-    def convert_response_to_dict(cls, response):
-      response_dict = {}
-      start = 0
-      text = response.text
-
-      for i, char in enumerate(text):
-        if char == "\n":
-          line = text[start:i].split("=")
-          if len(line) >= 2 and line[1] != "":
-            key, value = line
-            if key in response_dict.keys():
-              key += "_1"
-            response_dict[key] = value
-          start = i + 1
-
-      response_dict['status'] = response.status_code
-      return response_dict
-
-    @classmethod
+    @convert_response_to_dict
     def check_domain(cls, domain):
-      """ Checks availability of domain name"""
+      """ Checks availability of domain name. Takes single string as argument """
       # Create a copy of the original to prevent mutating it
       params = cls.PARAMS.copy()
       # Prepare query parameters
-      params['command'] = "CheckDomain"
+      params['command'] = 'CheckDomain'
       params['domain'] = domain
       # Send request and convert request to dictionary
-      response = requests.get(cls.API, params=params)
-      response = cls.convert_response_to_dict(response)
 
-      return response
+      return requests.get(cls.API, params=params)
+    
+    @classmethod 
+    @convert_response_to_dict
+    def check_domains(cls, domains):
+      """ Check availability of domain names. Takes list of strings as argument """
+      # Create a copy of the original to prevent mutating it
+      params = cls.PARAMS.copy()
+      params['command'] = 'checkDomains'
+      
+      #Loop over given domains and add it to the params dict
+      for i, domain in enumerate(domains):
+        params['domain{}'.format(i)] = domain
+        
+      return requests.get(cls.API, params=params)
+
+    @classmethod
+    @convert_response_to_dict
+    def status_domain(cls, domain):
+      """ """
+      params = cls.PARAMS.copy()
+      params['command'] = 'StatusDomain'
+      params['domain'] = domain
+      return requests.get(cls.API, params=params)
+    
+    
