578f08c9114030c271af883f2251a74f977b83d3
Stef <stefvanhouten@gmail.com>
2020-01-13 14:16:56 +0100

    Working on adding all API endpoints

--- app.py
+++ app.py
@@ -2,16 +2,23 @@ from classes.hexonet import Hexonet, Customer
 import pprint
 
 pp = pprint.PrettyPrinter(indent=4)
-customer = Customer('stef', 'van Houten', 'emmakade', 'leeuwarden', 'friesland', '8922', 'NL', '0640811668', 'stefvanhouten@gmail.com')
+customer = Customer('stef', 'van Houten', 'emmakade', 'leeuwarden', 'friesland', '8922', 'NL', '0612311154', 'stefvanhouten@gmail.com')
 
 hexonet = Hexonet()
-# pp.pprint(hexonet.check_domain('test.org'))
-# pp.pprint(hexonet.check_domains(['test.org', 'test1.org', 'test2.org', 'test3.org']))
-# pp.pprint(hexonet.status_domain('stef2.org'))
+domain = 'stef1.co'
+auth = 'Wb29L-dfGi4b'
+
+# pp.pprint(hexonet.check_domain(domain))
+# pp.pprint(hexonet.check_domains(['domain1.com', 'domain2.com', 'domain3.com', 'domain4.com']))
+# pp.pprint(hexonet.status_domain(domain))
 # pp.pprint(hexonet.add_domain('stef5.org', 1, customer, customer, customer, customer, ['nameserver1.com']))
 # pp.pprint(hexonet.delete_domain('stef4.org'))
-pp.pprint(hexonet.transfer_domain('stef1.co', 'Wb29L-dfGi4b', 'usertransfer'))
-# pp.pprint(hexonet.list_transfers_incoming())
-# pp.pprint(hexonet.list_transfers_outgoing())
-# pp.pprint(hexonet.list_owned_servers())
 
+
+# pp.pprint(hexonet.transfer_domain(domain, auth=auth, action='usertransfer'))
+# pp.pprint(hexonet.transfer_domain(domain, action='approve'))
+
+pp.pprint(hexonet.list_transfers_incoming())
+pp.pprint(hexonet.list_transfers_outgoing())
+# pp.pprint(hexonet.list_owned_servers())
+# pp.pprint(hexonet.unlock_domain_for_transfer('stef1.co', 0))
--- classes/hexonet.py
+++ classes/hexonet.py
@@ -14,7 +14,9 @@ class MyDecorator(object):
     """Support instance methods."""
     return functools.partial(self.__call__, obj)
 
-  def __call__(self, obj, *args, **kwargs):
+  def __call__(self, obj, 
+               *args, **kwargs
+               ):
     response = self.func(obj, *args, **kwargs)
     text = response.text
     response_dict = {}
@@ -68,7 +70,9 @@ class Hexonet(object):
     }
     
     @classmethod 
-    def build_params(cls, name, customer, params):
+    def build_params(cls, name, 
+                     customer, params
+                     ):
       if type(customer) == Customer:
         for key in customer.__dict__.keys():
           params['{}0{}'.format(name, key)] = customer.__dict__[key]
@@ -80,10 +84,74 @@ class Hexonet(object):
     
     @classmethod
     @convert_response_to_dict
+    def activate_domain_registration(cls, domain):
+      """ 
+      The ActivateDomainRegistration command is used to submit a domain registration request to the registry. 
+      This step is only required, if the registrar needs to interact before the registration request is being submitted 
+      to the regisitry ( e.g. for .NO domains a registration form is required upfront). 
+      """
+      params = cls.PARAMS.copy()
+      params['command'] = 'ActivateDomainRegistration'
+      params['domain'] = domain
+      return requests.get(cls.API, params=params)
+    
+    @classmethod
+    @convert_response_to_dict
+    def activate_domain_trade(cls, domain):
+      """ 
+      The ActivateDomainTrade command is used to approve a domain trade, 
+      which requires an activation through the registrar (e.g. for .AT domains upon receipt of required documentation). 
+      """
+      params = cls.PARAMS.copy()
+      params['command'] = 'ActivateDomainTrade'
+      params['domain'] = domain
+      return requests.get(cls.API, params=params)
+    
+    @classmethod
+    @convert_response_to_dict
+    def activate_domain_transfer(cls, domain, action, 
+                                 repository=None, trigger=None
+                                 ):
+      """
+      action:
+      - REQUEST
+      - APPROVE
+      - REJECT
+      - DENY
+       
+      The ActivateDomainTransfer command is used to approve or reject a domain transfer which requires the active 
+      participation of the registrar or repository owner (e.g. all gTLDs).
+      
+      As soon as a gLTD transfer is initiated, our system sends a FOA email to the registrant by default, 
+      which contains a confirmation link. This link leads to a whitelabeled site where the transfer can be activated.
+      If a reseller or a customer with its own domain repository does not want to use this FOA email function, 
+      he needs to use ActivateDomainTransfer to confirm the transfer. The command can only be executed by registrars 
+      or RegistrarOC customers. If you are no repository owner, you have to submit the respective repository and trigger code.
+      """
+      params = cls.PARAMS.copy()
+      params['command'] = 'ActivateDomainTransfer'
+      params['domain'] = domain
+      
+      if repository is not None:
+        params['repository'] = repository
+        
+      if trigger is not None:
+        params['trigger'] = trigger
+        
+      return requests.get(cls.API, params=params)
+    
+    @classmethod
+    @convert_response_to_dict
     def add_domain(cls, domain, period, 
                    owner_contact, admin_contact, tech_contact, 
-                   billing_contact, nameservers=None):
-      """ Used to register new domain names """
+                   billing_contact, nameservers=None
+                   ):
+      """ 
+      Used to register new domain names.
+      period should be an integer 
+      Use the customer object to add contacts to the request. 
+      Nameservers should be a list of nameservers you want to add.
+      """
       params = cls.PARAMS.copy()
       params['command'] = 'AddDomain'
       params['domain'] = domain
@@ -99,17 +167,28 @@ class Hexonet(object):
     
     @classmethod
     @convert_response_to_dict
-    def delete_domain(cls, domain):
-      """ Deletes domain based on url """
+    def add_domain_av_record(cls, domain, trigger=None, 
+                             host=None, checktype=None
+                             ):
+      """ 
+      The AddDomainAvRecord command is used to create a Authentication and Verification (A/V) Data record. 
+      A/V records are only required for the .PRO registry at this point in time. 
+      """
       params = cls.PARAMS.copy()
-      params['command'] = 'DeleteDomain'
+      params['command'] = 'AddDomainAvRecord'
       params['domain'] = domain
-      return requests.get(cls.API, params=params)
+      
+      if trigger is not None:
+        params['trigger'] = trigger
+        
+      if host is not None:
+        params['host'] = host
+        
+      if checktype is not None:
+        params['checktype'] = checktype
+      
+      return request.get(cls.API, params=params)
     
-    @classmethod
-    def status_domain(cls):
-      """ """
-
     @classmethod
     @convert_response_to_dict
     def check_domain(cls, domain):
@@ -136,6 +215,70 @@ class Hexonet(object):
         params['domain{}'.format(i)] = domain
         
       return requests.get(cls.API, params=params)
+    
+    @classmethod 
+    @convert_response_to_dict
+    def check_domain_transfer(cls, domain, auth=None):
+      """ 
+      The CheckDomainTransfer command allows you to review the details which are necessary to perform a 
+      successful transfer of a gTLD CNOBIN (i.e. eMail addresses, current registrar, domain status, ...).
+
+      It is also possible to validate an AUTH code for the following gTLDs: 
+      .aero, .asia, .biz, .ca, .cat, .com, .info, .mobi, .name, .net, .org, .tel. 
+      The result of the validation is shown in the property AUTHISVALID.
+      """
+      params = cls.PARAMS.copy()
+      params['command'] = 'CheckDomainTransfer'
+      if auth is not None:
+        params['auth'] = auth
+        
+      return requests.get(cls.API, params=params)
+    
+    @classmethod 
+    @convert_response_to_dict
+    def check_domain_av_record(cls, avid, repository):
+      """ 
+      The CheckDomainAvRecord command can be used used to check the availability of a 
+      authentication and verification (A/V) data record. 
+      A/V records are only required for the .PRO registry at this point in time.
+      """
+      params = cls.PARAMS.copy()
+      params['command'] = 'CheckDomainAvRecord'
+        
+      return requests.get(cls.API, params=params)
+    
+    @classmethod
+    @convert_response_to_dict
+    def delete_domain(cls, domain):
+      """ Deletes domain based on url """
+      params = cls.PARAMS.copy()
+      params['command'] = 'DeleteDomain'
+      params['domain'] = domain
+      return requests.get(cls.API, params=params)
+    
+    @classmethod
+    @convert_response_to_dict
+    def modify_domain(cls, domain):
+      """ TODO find out what are the most important commands for modifying and workout this function """
+      params = cls.PARAMS.copy()
+      params['command'] = 'ModifyDomain'
+      params['domain'] = domain
+      return requests.get(cls.API, params=params)
+    
+    @classmethod
+    @convert_response_to_dict
+    def pay_domain_renewal(cls, domain, period=None):
+      """
+      The PayDomainRenewal command is used to pay the renewal of a single domain. 
+      It sets the PAIDUNTILDATE of a domain according to the given renewal period.
+      If a domain has been paid (as given by PAIDUNTILDATE) it will be renewed automatically when registry's EXPIREDATE is reached.
+      """
+      params = cls.PARAMS.copy()
+      params['command'] = 'PayDomainRenewal'
+      params['domain'] = domain
+      if period is not None:
+        params['period'] = period
+      return requests.get(cls.API, params=params)
 
     @classmethod
     @convert_response_to_dict
@@ -149,7 +292,9 @@ class Hexonet(object):
     
     @classmethod
     @convert_response_to_dict
-    def transfer_domain(cls, domain, auth, action):
+    def transfer_domain(cls, domain, auth=None, 
+                        action=None, transferlock=None
+                        ):
       """ Transfer a domain. Action commands are one of the following:
           - REQUEST; request transfer
           - APPROVE; approve outgoing transfer
@@ -160,8 +305,14 @@ class Hexonet(object):
       params = cls.PARAMS.copy()
       params['command'] = 'TransferDomain'
       params['domain'] = domain
-      params['auth'] = auth
-      params['action'] = action
+      
+      if auth is not None:
+        params['auth'] = auth
+      if action is not None:
+        params['action'] = action
+      if transferlock is not None:
+        params['transferlock'] = transferlock
+        
       return requests.get(cls.API, params=params)
     
     
@@ -189,3 +340,12 @@ class Hexonet(object):
       params['command'] = 'QueryDomainList'
       return requests.get(cls.API, params=params)
     
+    @classmethod
+    @convert_response_to_dict
+    def unlock_domain_for_transfer(cls, domain, shouldLock):
+      """ Takes integer 0 to unlock 1 to lock """
+      params = cls.PARAMS.copy()
+      params['domain'] = domain
+      params['command'] = 'ModifyDomain'
+      params['transferlock'] = shouldLock
+      return requests.get(cls.API, params=params)
